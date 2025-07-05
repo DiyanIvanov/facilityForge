f914ffb9fc8a69e56a53cee5204c9cad2172cc6a
Diyan Ivanov <coldys_@hotmail.com>
2025-07-05 21:31:07 +0100

    feat: add dashboard url

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from common.views import IndexPageView, AboutPageView
+from common.views import IndexPageView, AboutPageView, DashboardView
 
 urlpatterns = [
     path('', IndexPageView.as_view(), name='index'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
