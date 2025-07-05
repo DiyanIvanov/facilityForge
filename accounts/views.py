957f540be828fbee5fab5d15abc57351f1dddc97
Diyan Ivanov <coldys_@hotmail.com>
2025-07-05 19:13:52 +0100

    feat: add forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,19 @@
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
 
 
+
+UserModel = get_user_model()
 
 class CustomLoginView(LoginView):
     template_name = 'accounts/login-page.html'
     redirect_authenticated_user = True
 
+
+class RegisterView(CreateView):
+    model = UserModel
+    form_class = UserCreationForm
+    template_name = 'accounts/register-page.html'
+    fields = ['username', 'email', 'password']
