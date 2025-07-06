1021923a35766ef6c639cb60b675a1a836e04ada
Diyan Ivanov <coldys_@hotmail.com>
2025-07-06 18:51:02 +0100

    feat: register user model on admin page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from accounts.models import FacilityForgeUser
 
-# Register your models here.
+
+@admin.register(FacilityForgeUser)
+class FacilityForgeUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', )
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    list_filter = ('username',)
+    ordering = ('username',)
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'email', 'password')
+        }),
+        ('Personal Info', {
+            'fields': ('first_name', 'last_name', 'phone_number')
+        }),
+    )
