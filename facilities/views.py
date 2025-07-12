4021d021304930953b861d06f03e56207d7665f2
Diyan Ivanov <coldys_@hotmail.com>
2025-07-12 21:10:41 +0100

    feat: add facilities view

--- facilities/views.py
+++ facilities/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class FacilityManagement(TemplateView):
+    template_name = 'account_management/facilities.html'
+    extra_context = {
+        'title': 'Facilities',
+        'description': 'Manage your facilities efficiently with FacilityForge. Add, edit, and view details of your facilities.'
+    }
