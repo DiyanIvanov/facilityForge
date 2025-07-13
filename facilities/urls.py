6901e930abb0189777a6aeec6600c0021d217e24
Diyan Ivanov <coldys_@hotmail.com>
2025-07-13 21:25:39 +0100

    feat: add url for facility details

--- facilities/urls.py
+++ facilities/urls.py
@@ -5,5 +5,6 @@ from facilities.views import FacilityManagement, CreateFacility
 urlpatterns = [
     path('', FacilityManagement.as_view(), name='facilities'),
     path('create/', CreateFacility.as_view(), name='create-facility'),
+    # path('<int:pk>/', FacilityDetails.as_view(), name='facility-detail'),
 
 ]
