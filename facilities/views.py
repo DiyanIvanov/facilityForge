3d3385d61de2280ea95f0b8e0c794e724fc1f256
Diyan Ivanov <coldys_@hotmail.com>
2025-07-13 11:36:24 +0100

    feat: add facilities to contex on dashboard view

--- facilities/views.py
+++ facilities/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, CreateView
+
+from facilities.forms import CreateFacilityForm
 
 
 class FacilityManagement(TemplateView):
-    template_name = 'account_management/facilities.html'
-    extra_context = {
-        'title': 'Facilities',
-        'description': 'Manage your facilities efficiently with FacilityForge. Add, edit, and view details of your facilities.'
-    }
+    template_name = 'facilities/facilities.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['facilities'] = self.request.user.owned_facilities.all()
+        return context
+
+
+class CreateFacility(CreateView):
+    template_name = 'facilities/create-facility.html'
+    form_class = CreateFacilityForm
+    success_url = reverse_lazy('facilities')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        form.instance.manager = self.request.user
+        return super().form_valid(form)
