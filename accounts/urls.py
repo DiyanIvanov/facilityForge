2600b05cdb190fa134bbf57bdb1c1ea7c72a15bf
Diyan Ivanov <coldys_@hotmail.com>
2025-07-13 12:51:30 +0100

    feat: add edit user url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LogoutView
-from django.urls import path
-from accounts.views import CustomLoginView, RegisterView
+from django.urls import path, include
+from accounts.views import CustomLoginView, RegisterView, EditProfileView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('account/', include([
+        path('', EditProfileView.as_view(), name='edit-profile'),
+    ]))
 ]
