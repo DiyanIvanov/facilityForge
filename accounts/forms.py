2ea8a63c396ff28d27f85ddab989b3cf0297442a
Diyan Ivanov <coldys_@hotmail.com>
2025-07-13 12:51:10 +0100

    feat: add edit user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,22 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 
 class  CustomRegisterForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = get_user_model()
         fields = ('username', 'email')
+
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
