df4dff99961bec392ad2bdc938f6d87dd832b792
Diyan Ivanov <coldys_@hotmail.com>
2025-07-15 20:36:20 +0100

    feat: add teams view

--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView, FormView, ListView
 
-from accounts.forms import CustomRegisterForm, EditProfileForm
+from accounts.forms import CustomRegisterForm, EditProfileForm, CreateTeamForm
+from accounts.models import Team
 
 UserModel = get_user_model()
 
@@ -30,3 +31,25 @@ class EditProfileView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class TeamsView(LoginRequiredMixin, ListView):
+    template_name = 'teams/teams.html'
+    model = Team
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['teams'] = self.request.user.owned_teams.all()
+        return context
+
+class CreateTeam(LoginRequiredMixin, CreateView):
+    template_name = 'teams/create-team.html'
+    form_class = CreateTeamForm
+    success_url = reverse_lazy('teams')
+
+    def form_valid(self, form):
+        form.instance.team_owner = self.request.user
+        form.instance.manager = self.request.user
+        return super().form_valid(form)
+
+
