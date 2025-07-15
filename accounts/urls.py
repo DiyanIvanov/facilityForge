dec69488d96ce462415a478ab34396071bf72613
Diyan Ivanov <coldys_@hotmail.com>
2025-07-15 20:37:58 +0100

    feat: add teams urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path, include
-from accounts.views import CustomLoginView, RegisterView, EditProfileView
+from accounts.views import CustomLoginView, RegisterView, EditProfileView, CreateTeam, TeamsView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
@@ -8,5 +8,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('account/', include([
         path('', EditProfileView.as_view(), name='edit-profile'),
-    ]))
+
+    ])),
+    path('teams/', include([
+    path('', TeamsView.as_view(), name='teams'),
+        path('create/', CreateTeam.as_view(), name='create-team'),
+    ])),
 ]
