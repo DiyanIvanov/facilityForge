7e286426e68049ed573841fa9a8ff10779757528
Diyan Ivanov <coldys_@hotmail.com>
2025-07-16 17:16:20 +0100

    feat: create applications view

--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView
+
+from applications.models import Applications
+
 
 # Create your views here.
+class ApplicationView(LoginRequiredMixin, ListView):
+    template_name = 'applications/applications.html'
+    model = Applications
+    context_object_name = 'applications'
+
+    def get_queryset(self):
+        return Applications.objects.filter(applicant=self.request.user).order_by('-id')
