ebba105c10ef4315c9de9cfedbe80b719775403c
Diyan Ivanov <coldys_@hotmail.com>
2025-07-19 20:14:39 +0100

    feat: make description field mandatory

--- applications/models.py
+++ applications/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Applications(models.Model):
     application_id = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField()
     status = models.CharField(
         max_length=20,
         choices=[
