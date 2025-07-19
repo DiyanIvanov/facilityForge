29e432d79c3509d9703c0c7e5d2c827d8ee1f522
Diyan Ivanov <coldys_@hotmail.com>
2025-07-19 19:23:16 +0100

    style: add dynamic searching view url

--- applications/urls.py
+++ applications/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from applications.views import ApplicationView, AcceptOrRejectApplicationView
+from applications.views import ApplicationView, AcceptOrRejectApplicationView, SearchFacilitiesAndTeamsView
 
 urlpatterns = [
     path('', ApplicationView.as_view(), name='applications'),
     path('accept-reject/', AcceptOrRejectApplicationView.as_view(), name='accept_or_reject_application'),
+    path('search/', SearchFacilitiesAndTeamsView.as_view(), name='search_facilities_and_teams'),
 ]
