2073d9e236b48d814b57580e773be56e1e1c7457
Diyan Ivanov <coldys_@hotmail.com>
2025-07-19 16:45:55 +0100

    feat: add Accept or reject view url

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from applications.views import ApplicationView
+from applications.views import ApplicationView, AcceptOrRejectApplicationView
 
 urlpatterns = [
     path('', ApplicationView.as_view(), name='applications'),
+    path('accept-reject/', AcceptOrRejectApplicationView.as_view(), name='accept_or_reject_application'),
 ]
