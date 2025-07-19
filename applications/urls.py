d0497d3a49661b7786e2c013d625cf4eee9d3e08
Diyan Ivanov <coldys_@hotmail.com>
2025-07-19 20:16:15 +0100

    feat: add team application url

--- applications/urls.py
+++ applications/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 
-from applications.views import ApplicationView, AcceptOrRejectApplicationView, SearchFacilitiesAndTeamsView
+from applications.views import ApplicationsView, AcceptOrRejectApplicationView, SearchFacilitiesAndTeamsView, \
+    TeamApplicationView
 
 urlpatterns = [
-    path('', ApplicationView.as_view(), name='applications'),
+    path('', ApplicationsView.as_view(), name='applications'),
     path('accept-reject/', AcceptOrRejectApplicationView.as_view(), name='accept_or_reject_application'),
     path('search/', SearchFacilitiesAndTeamsView.as_view(), name='search_facilities_and_teams'),
+    path('<int:pk>/', include([
+        path('team-application/', TeamApplicationView.as_view(), name='team_application'),
+    ])),
 ]
