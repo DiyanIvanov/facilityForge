0d0b04034a0166b216f291778ca368690e7e1bae
Diyan Ivanov <coldys_@hotmail.com>
2025-07-19 16:45:44 +0100

    feat: add Accept or reject view

--- applications/views.py
+++ applications/views.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, FormView, TemplateView
 
+from applications.forms import ApplicationForm
 from applications.models import Applications
 
 
@@ -12,3 +15,41 @@ class ApplicationView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Applications.objects.filter(applicant=self.request.user).order_by('-id')
+
+
+
+class AcceptOrRejectApplicationView(LoginRequiredMixin, FormView):
+    form_class = ApplicationForm
+    template_name = 'applications/applications.html'
+
+    def form_valid(self, form):
+        application_id = form.cleaned_data['application_id']
+        action = form.cleaned_data['action']
+        try:
+            application = Applications.objects.get(application_id=application_id)
+
+            if action == 'accept':
+                application.status = 'approved'
+            elif action == 'reject':
+                application.status = 'rejected'
+
+            application.save()
+            return JsonResponse({
+                'success': True,
+                'status': application.status,
+                'id': application_id
+            })
+
+        except Applications.DoesNotExist:
+            return JsonResponse({
+                'success': False,
+                'error': 'Application not found',
+                'id': application_id
+            }, status=404)
+
+    def form_invalid(self, form):
+        return JsonResponse({
+            'success': False,
+            'error': 'Invalid form submission',
+            'form_errors': form.errors
+        }, status=400)
