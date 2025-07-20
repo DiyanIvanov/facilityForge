606a602bfa2390268652072e05510ff1162f69ad
Diyan Ivanov <coldys_@hotmail.com>
2025-07-20 09:14:28 +0100

    feat: split facility and teams application form, allows facility form to have team filed where the user can choose some of his/her own teams to apply as.

--- applications/forms.py
+++ applications/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.db.models import Q
+from django.apps import apps
 from applications.models import Applications
 
 
@@ -12,8 +14,27 @@ class ApplicationForm(forms.Form):
     )
 
 
-class TeamOrFacilityApplicationForm(forms.ModelForm):
+class TeamApplicationForm(forms.ModelForm):
     class Meta:
         model = Applications
         fields = ('description',)
 
+
+
+class FacilityApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Applications
+        fields = ('description', 'team')
+
+
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user', None)
+        super().__init__(*args, **kwargs)
+
+        if user:
+            self.fields['team'].queryset = apps.get_model('accounts', 'Team').objects.filter(
+                Q(team_owner=user) | Q(manager=user)
+            ).distinct()
+            self.fields['team'].required = False
+            self.fields['team'].label = "Apply as team (optional)"
+            self.fields['team'].widget.attrs.update({'class': 'form-select'})
