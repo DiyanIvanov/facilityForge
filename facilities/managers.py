27fcadea1d281fe179fae4cecd616ee6d526e6a6
Diyan Ivanov <coldys_@hotmail.com>
2025-07-20 21:41:12 +0100

    feat: add custom facility manager

--- facilities/managers.py
+++ facilities/managers.py
@@ -2,10 +2,27 @@ from django.db.models import  Manager, Q
 
 
 class FacilityManager(Manager):
-    def facilities_user_is_not_involved_in(self, user):
-        query = Q(owner=user) | Q(manager=user) | Q(tenants=user) | Q(engineering_teams__members=user) \
-                | Q(engineering_teams__team_owner=user) | Q(engineering_teams__manager=user) \
-                | Q(engineering_teams__applications__status='accepted') | Q(engineering_teams__applications__status='pending') \
-                | Q(applications__status='accepted') | Q(applications__status='pending')
 
-        return self.exclude(query).distinct()
+    def facilities_user_is_not_involved_in(self, user):
+        user_teams = user.owned_teams.all() | user.managed_teams.all()
+        team_ids = user_teams.values_list('id', flat=True)
+
+        direct_involvement = (
+                Q(owner=user) |
+                Q(manager=user) |
+                Q(tenants=user) |
+                Q(engineering_teams__in=team_ids)
+        )
+
+        # user_applied = Q(
+        #     applications__applicant=user,
+        #     applications__team__isnull=True,
+        #     applications__status__in=['pending', 'approved']
+        # )
+        #
+        # team_applied = Q(
+        #     applications__team__in=team_ids,
+        #     applications__status__in=['pending', 'approved']
+        # )
+
+        return self.exclude(direct_involvement).distinct()
