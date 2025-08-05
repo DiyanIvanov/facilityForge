564773b2721ac4bbde25bb06a71cc3ac9312d72a
Diyan Ivanov <coldys_@hotmail.com>
2025-08-05 19:35:56 +0100

    feat: add user application eligibility check

--- accounts/managers.py
+++ accounts/managers.py
@@ -10,3 +10,13 @@ class TeamManager(Manager):
     def all_user_teams(self, user):
         query = Q(team_owner=user) | Q(members=user) | Q(manager=user)
         return self.filter(query).distinct()
+
+    def is_user_allowed_to_apply(self, user, team):
+        team = self.get(pk=team.pk)
+        if team.team_owner == user or team.manager == user:
+            return False
+        if team.applications.filter(applicant=user, status__in=['pending']).exists():
+            return False
+        if team.members.filter(pk=user.pk).exists():
+            return False
+        return True
